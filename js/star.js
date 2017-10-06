429d3a617bce9cd859375eea68fc0892df8e715c
Ravi Kumar L <upman16@gmail.com>
2017-10-06 12:26:35 +0530

    Cleanup

--- js/star.js
+++ js/star.js
@@ -1,48 +1,100 @@
+var constants = {
+    starPointRadius: 5,
+    numPointsOnStar: 12,
+    color: [70, 130, 180],
+    lineStrokeWeight: 2,
+    pointStrokeWeight: 1
+};
+
+function setupConstants() {
+    constants.starAnglePerPoint = (2 * Math.PI) / constants.numPointsOnStar;
+    constants.screenCenterX = windowWidth / 2;
+    constants.screenCenterY = windowHeight / 2;
+    constants.starOuterRadius = Math.min(windowWidth, windowHeight) * 0.35;
+    constants.starInnerRadius = Math.min(windowWidth, windowHeight) * 0.2;
+    // mouseX and mouseY are set to 0 before a mouseMoved event is fired on the page.
+    // Shows an ugly image that way
+    // Ugly fix below for a pretty image
+    mouseX = constants.screenCenterX + 1;
+    mouseY = constants.screenCenterY;
+}
+
+function getAngleWithXAxis(originX, originY, pointX, pointY) {
+    v1 = createVector(1, 0); // x-axis
+    v2 = createVector(pointX - originX, pointY - originY); // Line to the point from the center
+    angle = v1.angleBetween(v2); // This gives an angle between 0 and Pi radians, But we need this between 0 and 2 Pi
+    // The hack below to work out this kink
+    if(pointY > originY) {
+        angle = -(2 * Math.PI - angle);
+    } else {
+        angle = -angle;
+    }
+
+    return angle;
+}
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    setupConstants();
 }
 
 function draw() {
 
     clear();
-    var NumPointsOnStar = 15;
-    var screenCenterX = windowWidth / 2;
-    var screenCenterY = windowHeight / 2;
-    var starOuterRadius = Math.min(windowWidth, windowHeight) * 0.35;
-    var starInnerRadius = Math.min(windowWidth, windowHeight) * 0.2;
-    var starPointRadius = 5;
-    v1 = createVector(1, 0);
-    v2 = createVector(mouseX - screenCenterX, mouseY - screenCenterY);
-    var mouseAngle = v1.angleBetween(v2);
-    var innerStarAnglePartition = (2 * Math.PI) / NumPointsOnStar;
-    var outerStarAnglePartition = (2 * Math.PI) / (NumPointsOnStar);
-    fill(70,130,180);
-    stroke(70,130,180);
-    strokeWeight(1);
+    fill.apply(this, constants.color);
+    stroke.apply(this, constants.color);
+    strokeWeight(constants.pointStrokeWeight);
     innerStarPoints = [];
     outerStarPoints = [];
-    for( var i = 0; i < NumPointsOnStar; i++) {
+    var currentMouseAngle = getAngleWithXAxis(constants.screenCenterX, constants.screenCenterY, mouseX, mouseY);
+    for( var i = 0; i < constants.numPointsOnStar; i++) {
         innerStarPoints.push({
-             x: (starInnerRadius * Math.cos(i * innerStarAnglePartition )) + screenCenterX,
-             y: (starInnerRadius * Math.sin(i * innerStarAnglePartition )) + screenCenterY
+             x: (
+                    constants.starInnerRadius *
+                    Math.cos(
+                        (constants.starAnglePerPoint / 2) +
+                        ( i * constants.starAnglePerPoint )
+                    )
+                ) + constants.screenCenterX,
+             y: (
+                    constants.starInnerRadius *
+                    Math.sin(
+                        (constants.starAnglePerPoint / 2) +
+                        ( i * constants.starAnglePerPoint )
+                    )
+                ) + constants.screenCenterY
         });
         outerStarPoints.push({
-            x: (starOuterRadius * Math.cos( (-outerStarAnglePartition / 2) +  mouseAngle +  (i * outerStarAnglePartition) )) + screenCenterX,
-            y: (starOuterRadius * Math.sin((-outerStarAnglePartition / 2) + mouseAngle + (i * outerStarAnglePartition) )) + screenCenterY
+            x: (
+                    constants.starOuterRadius *
+                    Math.cos(
+                        currentMouseAngle +
+                        (i * constants.starAnglePerPoint)
+                    )
+                ) + constants.screenCenterX,
+            y: (
+                    constants.starOuterRadius *
+                    Math.sin(
+                        currentMouseAngle +
+                        (i * constants.starAnglePerPoint)
+                    )
+                ) + constants.screenCenterY
         });
-        ellipse(innerStarPoints[i].x, innerStarPoints[i].y, starPointRadius);
-        ellipse(outerStarPoints[i].x, outerStarPoints[i].y, starPointRadius);
+        ellipse(innerStarPoints[i].x, innerStarPoints[i].y, constants.starPointRadius);
+        ellipse(outerStarPoints[i].x, outerStarPoints[i].y, constants.starPointRadius);
     }
 
     noFill();
-    strokeWeight(2);
+    strokeWeight(constants.lineStrokeWeight);
     for(var i = 0; i < outerStarPoints.length; i++) {
         var beforeIndex = (i === 0)? innerStarPoints.length - 1 : i - 1;
         line(outerStarPoints[i].x, outerStarPoints[i].y, innerStarPoints[beforeIndex].x, innerStarPoints[beforeIndex].y);
         line(outerStarPoints[i].x, outerStarPoints[i].y, innerStarPoints[i].x, innerStarPoints[i].y);
     }
+
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+    setupConstants();
+    resizeCanvas(windowWidth, windowHeight);
 }
